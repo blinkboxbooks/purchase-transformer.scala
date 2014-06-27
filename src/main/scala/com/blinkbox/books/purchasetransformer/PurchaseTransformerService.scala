14538be946088c8c8e2784dab25dc6e2713ddb27
Jan Stette <jans@blinkbox.com>
2014-06-27 19:20:13 +0100

    Get settings from config, use new message publisher for outgoing messages and errors.

--- src/main/scala/com/blinkbox/books/purchasetransformer/PurchaseTransformerService.scala
+++ src/main/scala/com/blinkbox/books/purchasetransformer/PurchaseTransformerService.scala
@@ -1,13 +1,17 @@
 package com.blinkbox.books.purchasetransformer
 
-import akka.actor.{ ActorSystem, Props }
+import akka.actor.{ ActorSystem, ActorRef, Props }
 import akka.io.IO
+import akka.pattern.ask
 import akka.util.Timeout
 import com.blinkbox.books.config.Configuration
+import com.blinkbox.books.config.RichConfig
 import com.blinkboxbooks.hermes.rabbitmq._
 import com.blinkboxbooks.hermes.rabbitmq.RabbitMqConsumer.QueueConfiguration
 import com.blinkbox.books.messaging._
+import com.rabbitmq.client.Connection
 import com.typesafe.scalalogging.slf4j.Logging
+import java.util.concurrent.TimeUnit
 import spray.can.Http
 import scala.concurrent.duration._
 import scala.concurrent.Future
@@ -21,59 +25,85 @@ object PurchaseTransformerService extends App with Configuration with Logging {
 
   logger.info("Starting")
 
-  val connection = RabbitMq.reliableConnection(RabbitMqConfig(config))
-
-  // Initialise the actor system.
-  implicit val system = ActorSystem("reporting-service")
-  implicit val executionContext = system.dispatcher
-
-  val httpActor = IO(Http)
-
-  // Could use the Java client library instead?
-  implicit val timeout = Timeout(10.seconds) // TODO: Config
-  val bookDao = new HttpBookDao(httpActor, "http:localhost/catalogue/books") // TODO: Config
-
-  // TODO: Replace these with RabbitMQ implementations when available.
-  class DummyMessageSender(exchangeName: String) extends EventPublisher {
-    override def publish(event: Event): Future[Unit] = Future {
-      println(s"* PRETEND publishing event: $event to $exchangeName")
-    }
-  }
-  class DummyErrorHandler(queueName: String) extends ErrorHandler {
-    override def handleError(message: Event, error: Throwable): Future[Unit] = Future {
-      println(s"* PRETEND handling error: ${error.getMessage} to $queueName")
-    }
-  }
-
   val serviceConf = config.getConfig("service.purchaseTransformer")
   logger.info(s"Starting purchase-transformer service with config: $serviceConf")
 
-  val invalidMessagesForClubcardsQueue = "Clubcard.Listener.DLQ"
-  val outgoingExchangeForClubcardMessages = "Clubcard.Collector.Exchange"
-  val clubcardMessageSender = new DummyMessageSender(outgoingExchangeForClubcardMessages)
-  val clubcardMsgErrorHandler = new DummyErrorHandler(invalidMessagesForClubcardsQueue)
+  // Use separate conncections for consumers and publishers.
+  def newConnection() = RabbitMq.reliableConnection(RabbitMqConfig(config))
+  val publisherConnection = newConnection()
+  val consumerConnection = newConnection()
 
-  val invalidMessagesForEmailQueue = "Mail.Listener.DLQ"
-  val outgoingExchangeForEmailMessages = "Mail.Sender.Exchange"
-  val emailMessageSender = new DummyMessageSender(outgoingExchangeForEmailMessages)
-  val emailMsgErrorHandler = new DummyErrorHandler(invalidMessagesForEmailQueue)
+  // Initialise the actor system.
+  implicit val system = ActorSystem("reporting-service")
+  implicit val ec = system.dispatcher
+
+  val httpActor = IO(Http)
+
+  // Could use the Java Book client library instead?
+  val ClientRequestTimeout = Timeout(serviceConf.getDuration("clientRequestInterval", TimeUnit.SECONDS).seconds)
+  val bookDao = new HttpBookDao(httpActor, serviceConf.getHttpUrl("bookService.url").toString)(ClientRequestTimeout, ec)
+
+  val OutgoingMessageTimeout = serviceConf.getDuration("messagePublishingTimeout", TimeUnit.SECONDS).seconds
+  val ClientRequestRetryInterval = serviceConf.getDuration("clientRequestInterval", TimeUnit.SECONDS).seconds
+
+  // TODO: Should the routing key be made optional still, seeing as some services don't use it yet?
+  // I guess it doesn't matter if we publish messages on a topic; old-school consumers can just
+  // subscribe a topic of "*"?
+
+  def publisher(exchange: String, routingKey: String) =
+    system.actorOf(Props(new RabbitMqConfirmedPublisher(publisherConnection.createChannel(), exchange, routingKey, OutgoingMessageTimeout)))
+
+  val emailListenerConfig = serviceConf.getConfig("emailListener.input")
+  val outgoingExchangeForEmailMessages = emailListenerConfig.getString("exchangeName")
+  val emailPublisher = publisher(outgoingExchangeForEmailMessages, "TODO")
+  val emailErrorPublisher = publisher("", serviceConf.getString("emailListener.error"))
+
+  val clubcardListenerConfig = serviceConf.getConfig("clubcardListener.input")
+  val outgoingExchangeForClubcardMessages = clubcardListenerConfig.getString("exchangeName")
+  val clubcardPublisher = publisher(outgoingExchangeForEmailMessages, "TODO")
+  val clubcardErrorPublisher = publisher("", serviceConf.getString("clubcardListener.error"))
+
+  val invalidMessagesForClubcardsQueue = serviceConf.getString("clubcardListener.error")
+  val clubcardMessageSender = new PublishingMessageSender(clubcardPublisher)
+  val clubcardMsgErrorHandler = new PublishingErrorHandler(clubcardErrorPublisher)
+
+  val invalidMessagesForEmailQueue = serviceConf.getString("emailListener.error")
+  val emailMessageSender = new PublishingMessageSender(emailPublisher)
+  val emailMsgErrorHandler = new PublishingErrorHandler(emailErrorPublisher)
+
+  val routingId = emailListenerConfig.getString("emailRoutingInstance")
+  val templateName = emailListenerConfig.getString("templateName")
+  val retryInterval = serviceConf.getDuration("messagePublishingTimeout", TimeUnit.SECONDS).seconds
 
   // Create actors for email messages.
-  val routingId = "TODO" // TODO: Get from properties.
-  val templateName = "TODO" // TODO: Get from properties.
-  val retryInterval = 10.seconds // TODO: Get from properties.
   val emailMessageHandler = system.actorOf(Props(
     new EmailMessageHandler(bookDao, emailMessageSender, emailMsgErrorHandler, routingId, templateName, retryInterval)))
-  system.actorOf(Props(new RabbitMqConsumer(connection.createChannel,
-    QueueConfiguration(serviceConf.getConfig("emailListener.input")), "email-msg-consumer", emailMessageHandler)))
+  system.actorOf(Props(new RabbitMqConsumer(consumerConnection.createChannel,
+    QueueConfiguration(emailListenerConfig), "email-msg-consumer", emailMessageHandler)))
     .tell(RabbitMqConsumer.Init, null)
 
   // Create actors that handle clubcard point messages.
   val clubcardMessageHandler = system.actorOf(Props(
     new ClubcardMessageHandler(clubcardMessageSender, clubcardMsgErrorHandler, retryInterval)))
-  system.actorOf(Props(new RabbitMqConsumer(connection.createChannel,
-    QueueConfiguration(serviceConf.getConfig("clubcardListener.input")), "clubcard-msg-consumer", clubcardMessageHandler)))
+  system.actorOf(Props(new RabbitMqConsumer(consumerConnection.createChannel,
+    QueueConfiguration(clubcardListenerConfig), "clubcard-msg-consumer", clubcardMessageHandler)))
     .tell(RabbitMqConsumer.Init, null)
 
   logger.info("Started")
+
+  // For now: wrap actor in the MessageSender interface.
+  class PublishingMessageSender(publisherActor: ActorRef) extends EventPublisher {
+    implicit val timeout = Timeout(OutgoingMessageTimeout)
+    override def publish(event: Event): Future[Unit] = (publisherActor ? event).map(result => ())
+  }
+
+  // Ditto for the ErrorHandlers.
+  class PublishingErrorHandler(publisherActor: ActorRef) extends ErrorHandler {
+    implicit val timeout = Timeout(OutgoingMessageTimeout)
+    override def handleError(event: Event, e: Throwable): Future[Unit] = {
+      logger.error(s"Unrecoverable error in processing event: $event", e)
+      (publisherActor ? event).map(result => ())
+    }
+  }
+
 }
